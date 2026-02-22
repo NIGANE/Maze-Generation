0c9411901d8bcf16af802ca42314b7bca2560119
Achraf Merkht <amerkht@c4r2p4.1337.ma>
2026-02-22 21:33:05 +0000

    NEGANE

--- Parsing.py
+++ Parsing.py
@@ -1,4 +1,10 @@
+import random
+from typing import Union, Optional, List
 from pydantic import BaseModel, Field, model_validator
+
+
+def is_empty(s: str) -> bool:
+    return s == ""
 
 
 class Parsing(BaseModel):
@@ -9,10 +15,10 @@ class Parsing(BaseModel):
         ge=10, description="height conf should be greater than 10"
     )
     entry: tuple = Field(
-        description="enty should be valid cordinates (x,y)"
+        description="enty should be valid cordinates x,y"
     )
     exit: tuple = Field(
-        description="exit should be valid cordinates (x,y)"
+        description="exit should be valid cordinates x,y"
     )
     output_file: str = Field(
         max_length=10,
@@ -21,14 +27,36 @@ class Parsing(BaseModel):
     perfect: bool = Field(
         description="perfect argument should be valid boolian (True or False)"
     )
+    seed: Optional[int] = Field(
+        description="seed field should be valid number"
+    )
+    rn: Union[int, float, None]
 
     @staticmethod
     def get_conf(data: str) -> dict:
         conf: dict = {}
         filtered: list = data.split('\n')
+        filtered = [
+            ele.strip() for ele in filtered if not ele.strip().startswith('#')
+            and not is_empty(ele.strip())
+            ]
         for ele in filtered:
-            key: str = ele.strip().split('=')[0].lower()
-            val: str = ele.strip().split('=')[1]
+            if "#" in ele:
+                raise ValueError(
+                    "invalid configuration syntax, line contains '#'"
+                    )
+            elif ele.startswith('='):
+                raise ValueError(
+                    "invalid configuration syntax, line starts with '='")
+        req_conf: List[str] = [
+            'width', 'height', 'entry', 'exit', 'seed', 'perfect',
+            'output_file'
+            ]
+        for ele in filtered:
+            key: str = ele.strip().split('=')[0].lower().strip()
+            if key not in req_conf:
+                raise ValueError(f"invalid configuration param '{key}'")
+            val: str = ele.strip().split('=')[1].strip()
             conf[key] = val
         return conf
 
@@ -47,6 +75,18 @@ class Parsing(BaseModel):
     def finishing(s) -> 'Parsing':
         s.entry = s.to_int(s.entry)
         s.exit = s.to_int(s.exit)
+        random.seed() if s.seed is None else random.seed(s.seed)
+        s.rn = random.random()
+        if s.entry[0] > s.width or s.entry[1] > s.height:
+            raise ValueError(
+                f"entry cordinates ({s.entry[0]},{s.entry[1]}) out of "
+                f"maze range w={s.width}, h={s.height}"
+                )
+        if s.exit[0] > s.width or s.exit[1] > s.height:
+            raise ValueError(
+                f"exit cordinates ({s.exit[0]},{s.exit[1]}) out of "
+                f"maze range w={s.width}, h={s.height}"
+                )
         return s
 
     @model_validator(mode='before')
@@ -63,7 +103,7 @@ class Parsing(BaseModel):
         if 'entry' not in data:
             raise ValueError("no entry provided on configuration file")
         elif 'exit' not in data:
-            raise ValueError("no entry provided on configuration file")
+            raise ValueError("no exit provided on configuration file")
 
         data['width'] = int(data['width'])
         data['height'] = int(data['height'])
@@ -73,12 +113,15 @@ class Parsing(BaseModel):
             raise ValueError("the provided enty not valid")
         for ele in data['entry']:
             if not Parsing.valid_numbers(ele):
-                raise ValueError("the provided enty not valid")
+                raise ValueError("the provided entry not valid")
         for ele in data['exit']:
             if not Parsing.valid_numbers(ele):
-                raise ValueError("the provided enty not valid")
+                raise ValueError("the provided exit not valid")
         if len(data['exit']) != 2:
             raise ValueError("the provided exit not valid")
         if 'perfect' not in data:
             raise ValueError("perfect filed is required")
+        if 'seed' not in data:
+            data['seed'] = None
+        data['rn'] = None
         return data
